c7a373f8f35ed099f2f6b214c6223c8ba8b3eb20
franvazquezz <franvazquez2001@gmail.com>
2023-06-28 15:27:43 -0300

    deploy 2.0

--- src/DB_connection.js
+++ src/DB_connection.js
@@ -1,19 +1,19 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 const UserModel = require('./models/User');
 const FavoriteModel = require('./models/Favorite');
-
-
+const {
+   DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY,
+ } = process.env;
 // EJERCICIO 03
 // A la instancia de Sequelize le falta la URL de conexión. ¡Agrégala!
 // Recuerda pasarle la información de tu archivo '.env'.
 
 // URL ----> postgres://DB_USER:DB_PASSWORD@DB_HOST/rickandmorty
-const sequelize = new Sequelize(
-   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/rickandmorty`,
-   { logging: false, native: false }
-);
+ const sequelize = new Sequelize(DB_DEPLOY, {
+   logging: false, // set to console.log to see the raw SQL queries
+   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+ });
 
 // EJERCICIO 05
 // Debajo de este comentario puedes ejecutar la función de los modelos.
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const { conn } = require('./DB_connection')
 const server = require('./app');
-const PORT = 3001;
+require ('dotenv').config() 
+const PORT = process.env.PORT || 3001
 
 
-conn.sync({ force: true})
+conn.sync({ alter: true})
 
 
 server.listen(PORT, () => {console.log(`Server listening to juice wrld on port: ${PORT}`);});
